bd0cf85ea6cf208ff7597b6a7a9054e770d1caad
codelover755 <anilchidura@googlemail.com>
2025-04-02 18:34:03 +0100

    finish jumbledwords.py
    now points are lost when the show answer button is clicked(you can get negative points)

--- jumbledwords.py
+++ jumbledwords.py
@@ -1,37 +1,58 @@
-from email import message
 from tkinter import * 
 import random
 from tkinter import messagebox
-from turtle import color
 
 words = {
-    "apple":"papel","mango":"oangm","achieve":"hveecia"
+    "apple":"papel","mango":"oangm",\
+        "achieve":"hveecia"
 }
 
 window = Tk()
 window.config(bg="yellow")
 
-current_answer = ""
 current_word = ""
+correct_answer = ""
+points = 0
 def new_word():
-    global current_answer, current_word
+    global current_word,correct_answer
     correct_answer,current_word = random.choice(list(words.items()))
     print(current_word,correct_answer)
     label.config(text=current_word,font=("Helvetica",20),fg="blue",bg="yellow")
 
 def check_answer():
-    global current_answer
+    global correct_answer,points
+    print(correct_answer)
     a = answer_entry.get()
-    if current_answer == a:
+    if correct_answer == a:
         messagebox.showinfo("win","This is correct")
+        answer_entry.delete(0,END)
+        points = points + 1
+        display.config(text=str(points))
+        new_word()
     else:
         messagebox.showerror("loose","try again")
+        answer_entry.delete(0,END)
+
+def show_ans():
+    global correct_answer, points
+    messagebox.showinfo("Answer","The answer is {}".format(correct_answer))
+    points -= 1
+    display.config(text=str(points))
+    new_word()
+
 label = Label(window,text="")
 answer_entry = Entry(window)
 answer = Button(window,text="Click this to answer", command=check_answer,font="consolas",bg="red",fg="white")
 new_word()
+display = Label(window,text="")
+print(points)
+reset_button = Button(window,text="Change word",command=new_word,font="consolas",bg="white",fg="red")
+show_answer = Button(window,text="Show answer",command=show_ans,font="consolas",bg="white",fg="green")
 
+display.pack()
 label.pack()
 answer_entry.pack()
 answer.pack()
+reset_button.pack()
+show_answer.pack()
 window.mainloop()
