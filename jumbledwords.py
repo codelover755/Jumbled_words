f0fdd836c1366b2d150d8a650ac9f7a97d8d160f
codelover755 <anilchidura@googlemail.com>
2025-09-19 16:59:35 +0100

    update.py
    better colour scheme and more words

--- jumbledwords.py
+++ jumbledwords.py
@@ -3,12 +3,11 @@ import random
 from tkinter import messagebox
 
 words = {
-    "apple":"papel","mango":"oangm",\
-        "achieve":"hveecia"
+    "apple":"papel","mango":"oangm",        "achieve":"hveecia","cart":"rcat",        "petals":"seltap","mouse":"uosme","cloud":"ldouc",        "elephant":"ateplehen"
 }
 
 window = Tk()
-window.config(bg="yellow")
+window.config(bg="CornflowerBlue")
 
 current_word = ""
 correct_answer = ""
@@ -17,7 +16,7 @@ def new_word():
     global current_word,correct_answer
     correct_answer,current_word = random.choice(list(words.items()))
     print(current_word,correct_answer)
-    label.config(text=current_word,font=("Helvetica",20),fg="blue",bg="yellow")
+    label.config(text=current_word,font=("Helvetica",20),fg="gray0",bg="CornflowerBlue")
 
 def check_answer():
     global correct_answer,points
@@ -27,7 +26,7 @@ def check_answer():
         messagebox.showinfo("win","This is correct")
         answer_entry.delete(0,END)
         points = points + 1
-        display.config(text=str(points))
+        display.config(text="Score:"+str(points))
         new_word()
     else:
         messagebox.showerror("loose","try again")
@@ -37,15 +36,15 @@ def show_ans():
     global correct_answer, points
     messagebox.showinfo("Answer","The answer is {}".format(correct_answer))
     points -= 1
-    display.config(text=str(points))
+    display.config(text="Score:"+str(points))
     new_word()
 
 label = Label(window,text="")
 answer_entry = Entry(window)
 answer = Button(window,text="Click this to answer", command=check_answer,font="consolas",bg="red",fg="white")
 new_word()
-display = Label(window,text="")
-print(points)
+display = Label(window,text="Score:")
+print("Score:", points)
 reset_button = Button(window,text="Change word",command=new_word,font="consolas",bg="white",fg="red")
 show_answer = Button(window,text="Show answer",command=show_ans,font="consolas",bg="white",fg="green")
 
